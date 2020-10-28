b77e2376f32d5cdaf6e591af7885ceac986feeac
mattsq <matthewjosephsimmons@gmail.com>
2020-10-28 12:47:06 +1100

    More changes to improve download process

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -3,15 +3,17 @@ library(purrr)
 datasets <- c("ArrowHead",
               "BasicMotions", "BME", "Coffee",
               "ERing", "GunPoint", "ToeSegmentation1",
-              "UMD", "Wine", "Car", "Earthquakes", "Lightning2", "Lightning7")
+              "UMD", "Wine", "Car", "Earthquakes", "Lightning2",
+              "Lightning7", "ItalyPowerDemand", "MoteStrain",
+              "Plane")
 
 download_and_extract <- function(dataset) {
   url <- paste0("http://www.timeseriesclassification.com/Downloads/",dataset,".zip")
   download_path <- paste0("data/",dataset,".zip")
   train_file <-  paste0(dataset,"_TRAIN.arff")
   test_file <-  paste0(dataset,"_TEST.arff")
-  download.file(url, destfile = download_path)
-  unzip(download_path, files = c(train_file, test_file), exdir = "data")
+  if (!file.exists(download_path)) download.file(url, destfile = download_path)
+  if (!file.exists(train_file) & !file.exists(test_file)) unzip(download_path, files = c(train_file, test_file), exdir = "data")
 }
 
 map(datasets, download_and_extract)
